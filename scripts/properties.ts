6c65ee07350550ae9d9f5d13e43cd7df0ecc9409
shizuku86 <shizuku86710@gmail.com>
2026-02-12 02:39:09 +0900

    first commit

--- scripts/properties.ts
+++ scripts/properties.ts
@@ -5,17 +5,13 @@ import type { KairoAddonProperties } from "@kairo-js/router";
  * Modify and use where # is written at the end of the sentence as appropriate
  */
 export const properties: KairoAddonProperties = {
-    id: "werewolf-template", # // a-z & 0-9 - _
+    id: "werewolf-additionalroles-1", // a-z & 0-9 - _
     metadata: {
-        /** 製作者の名前 */
-        authors: [
-            //"shizuku86"
-        ],
+        authors: ["shizuku86", "Lemonesan123"],
     },
     header: {
-        name: "Kairo-template", #
-        description:
-            "A starter template for developing Minecraft Bedrock Werewolf games powered by Kairo.", #
+        name: "Werewolf Additional Roles IV",
+        description: "The fourth expansion pack introducing new roles to the Werewolf game.",
         version: {
             major: 1,
             minor: 0,
@@ -28,11 +24,11 @@ export const properties: KairoAddonProperties = {
     dependencies: [
         {
             module_name: "@minecraft/server",
-            version: "2.4.0", #
+            version: "2.5.0",
         },
         {
             module_name: "@minecraft/server-ui",
-            version: "2.0.0", #
+            version: "2.0.0",
         },
     ],
     /** 前提アドオン */
