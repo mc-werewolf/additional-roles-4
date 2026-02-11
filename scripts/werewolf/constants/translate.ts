f8590037866488eaf8d953032770e2f93d9e0113
lemone0706 <hayato0706m2@gmail.com>
2026-02-12 02:42:21 +0900

    Added Role : Ghost

---
+++ scripts/werewolf/constants/translate.ts
@@ -0,0 +1,13 @@
+export const WEREWOLF_ROLEMONE_PACK_TRANSLATE_IDS = {
+    TITLE: "werewolf-rolemone-pack.name",
+    DESCRIPTION: "werewolf-rolemone-pack.description",
+    ROLE_NAME_GHOST: "werewolf-rolemone-pack.role.name.ghost",
+
+    ROLE_DESCRIPTION_GHOST: "werewolf-rolemone-pack.role.description.ghost",
+
+    GHOST_SKILL_START_MESSAGE: "werewolf-rolemone-pack.skill.message.ghost.start",    
+    GHOST_SKILL_RETURN_MESSAGE: "werewolf-rolemone-pack.skill.message.ghost.return",
+    GHOST_SKILL_SEARCH_NOTBODY_MESSAGE: "werewolf-rolemone-pack.skill.message.ghost.search.notbody",
+    GHOST_SKILL_NOTBODY_DEATH_MESSAGE: "werewolf-rolemone-pack.skill.message.ghost.notbody.deadlog",
+    GHOST_SKILL_NAME: "werewolf-rolemone-pack.skill.name.ghost"
+};
--- scripts/werewolf/skills/skillHandlers.ts
+++ scripts/werewolf/skills/skillHandlers.ts
@@ -1,5 +1,8 @@
+import { ghostSkillHandlers } from "./ghost";
 import { DefinitionRegistry, type GameEventHandlerMap } from "@mc-werewolf/game-module";
 
-export const roleSkillHandlers: Record<string, GameEventHandlerMap> = {};
+export const roleSkillHandlers: Record<string, GameEventHandlerMap> = {
+    ghost: ghostSkillHandlers
+};
 
 DefinitionRegistry.roleSkillHandlers.register(roleSkillHandlers);
